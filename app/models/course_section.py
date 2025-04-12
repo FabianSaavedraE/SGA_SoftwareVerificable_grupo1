991da6b220b4d77b8fb10cc59bbe1584b1962ef1
Chiara Romanini <caromanini@miuandes.cl>
2025-04-12 19:16:40 -0400

    fix: Fixed CourseSection dependencies to fit CourseInstance instead of Course

--- app/models/course_section.py
+++ app/models/course_section.py
@@ -1,11 +1,15 @@
 from app import db
+from sqlalchemy import Enum
 
 class CourseSection(db.Model):
     __tablename__ = 'course_sections'
 
     id = db.Column(db.Integer, primary_key=True)
     nrc = db.Column(db.String(50), nullable=False)
-    semester = db.Column(db.String(50), nullable=False)
+    overall_ponderation_type = db.Column(
+        db.Enum('Porcentaje', 'Peso', name='ponderation_type_enum'),
+        nullable=False
+    )
 
     course_instance_id = db.Column(db.Integer, db.ForeignKey('course_instances.id'), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
