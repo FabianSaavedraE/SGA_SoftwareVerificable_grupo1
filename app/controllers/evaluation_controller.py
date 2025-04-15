e394b599dc99dd3baa354dde36c09999998a1002
FabianSaavedraE <fasaavedra1@miuandes.cl>
2025-04-14 22:58:35 -0400

    feat: first interpretation of deafault ponderation is the same for all evaluations

--- app/controllers/evaluation_controller.py
+++ app/controllers/evaluation_controller.py
@@ -14,17 +14,35 @@ def getEvaluation(evaluation_id):
     return evaluation
 
 def createEvaluation(data):
+    from app.models.evaluation_type import EvaluationType
+
+    evaluation_type_id = data.get('evaluation_type_id')
+    evaluation_type = EvaluationType.query.get(evaluation_type_id)
+
+    if not evaluation_type:
+        return None  # Fallback defensivo
+
+    existing_evaluations = Evaluation.query.filter_by(evaluation_type_id=evaluation_type_id).all()
+    ponderation = data.get('ponderation')
+
+    if ponderation is '':
+        if evaluation_type.ponderation_type == 'Peso':
+            ponderation = 1
+        elif evaluation_type.ponderation_type == 'Porcentaje':
+            total_evaluations_after_creation = len(existing_evaluations) + 1
+            ponderation = round(100 / total_evaluations_after_creation, 2)
+
     new_evaluation = Evaluation(
         name = data.get('name'),
-        ponderation = data.get('ponderation'),
-        optional = data.get('optional'),
-        evaluation_type_id = data.get('evaluation_type_id')
+        ponderation = ponderation,
+        optional = data.get('optional', False),
+        evaluation_type_id = evaluation_type_id
     )
+
     db.session.add(new_evaluation)
     db.session.commit()
 
     return new_evaluation
-
 def updateEvaluation(evaluation, data):
     if not evaluation:
         return None
